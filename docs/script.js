7e15fa5b4f052f3cefe6e509a1a17bedf9b98ffc
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2025-02-25 19:52:06 +0200

    reset on burger menu on change

--- docs/script.js
+++ docs/script.js
@@ -1,30 +1,33 @@
 function initEventListeners() {
     const burgerMenuElement = document.querySelector('.burger-menu');
+    const mobileMediaQuery = window.matchMedia('(max-width: 700px)');
     
     burgerMenuElement.addEventListener('click', onBurgerClick);
+    mobileMediaQuery.addEventListener('change', onMediaChange)
 }
 
+//reset on burger menu and navigation on change
+function onMediaChange() {
+    const burgerMenuElement = document.querySelector('.burger-menu');
+    const navmenu = document.querySelector('.navigation-menu');
+    burgerMenuElement.classList.replace('close-menu', 'open-menu');
+    navmenu.style.display = '';
+}
 
 
 function onBurgerClick() {
     const burgerMenuElement = document.querySelector('.burger-menu');
     const navmenu = document.querySelector('.navigation-menu');
-
     
     //toggle burger menu
-    
+
     if (burgerMenuElement.classList.contains('open-menu')) {
         burgerMenuElement.classList.replace('open-menu', 'close-menu');
         navmenu.style.display = 'block';
     } else {
         burgerMenuElement.classList.replace('close-menu', 'open-menu');
-        navmenu.style.display = 'none';
+        navmenu.style.display = '';
     }
-
-    //change burger menu
-    // burgerMenuElement.classList.remove('open-menu');
-    // burgerMenuElement.classList.add('close-menu');
-                                           
 }
 
 initEventListeners();
