828692549dccc8e749a9a3544ac0e5e5804920a9
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2025-02-25 18:26:36 +0200

    show close icon on burger menu click

--- docs/script.js
+++ docs/script.js
@@ -7,9 +7,15 @@ function initEventListeners() {
 
 
 function onBurgerClick() {
+    const burgerMenuElement = document.querySelector('.burger-menu');
     const navmenu = document.querySelector('.navigation-menu');
 
+    //show navigation
     navmenu.style.display = "block";
+
+    //change burger menu
+    burgerMenuElement.style.backgroundImage =  'url("icons8-x-35.png")';
+                                           
 }
 
 initEventListeners();
