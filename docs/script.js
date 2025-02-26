ae0a7c6b21f190f112a8f990132f720278e7f75b
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2025-02-26 20:11:24 +0200

    add burger menu to every page working great

--- docs/script.js
+++ docs/script.js
@@ -3,6 +3,7 @@ function initEventListeners() {
     const mobileMediaQuery = window.matchMedia('(max-width: 700px)');
     
     burgerMenuElement.addEventListener('click', onBurgerClick);
+    burgerMenuElement.addEventListener('click', onBurgerClick2);
     mobileMediaQuery.addEventListener('change', onMediaChange)
 }
 
@@ -15,18 +16,37 @@ function onMediaChange() {
 }
 
 
+function onBurgerClick2() {
+    const headerContainerContent = document.querySelector('.header-container-height-without-home-page');
+
+    //toggle burger menu
+
+    if (headerContainerContent.classList.contains('header-container-height-without-home-page-close-menu')) {
+        headerContainerContent.classList.replace('header-container-height-without-home-page-close-menu', 'header-container-height-without-home-page-open-menu');
+        // headerContainerContent.style.heigh = "300px";
+        
+    } else {
+        headerContainerContent.classList.replace('header-container-height-without-home-page-open-menu', 'header-container-height-without-home-page-close-menu');
+    }
+}
+
 function onBurgerClick() {
     const burgerMenuElement = document.querySelector('.burger-menu');
     const navmenu = document.querySelector('.navigation-menu');
-    
+    const headerContainerContent = document.querySelector('.header-container-content-margin-home-page');
+
+
     //toggle burger menu
 
     if (burgerMenuElement.classList.contains('open-menu')) {
         burgerMenuElement.classList.replace('open-menu', 'close-menu');
         navmenu.style.display = 'block';
+        headerContainerContent.style.marginBottom = "200px";
+        
     } else {
         burgerMenuElement.classList.replace('close-menu', 'open-menu');
         navmenu.style.display = '';
+        headerContainerContent.style.marginBottom = '';
     }
 }
 
